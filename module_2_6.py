370555682141cb6dd9bcba3a4b7c8a55de99539f
Osotov-s <82163862+Osotov-s@users.noreply.github.com>
2024-12-06 23:34:27 +0500

    Update module_2_6.py

--- module_2_6.py
+++ module_2_6.py
@@ -1,15 +1,17 @@
 from random import randint
-
 r = (randint(3, 20))
 
-result = 0
+def get_password(r):
+    password = ''
+    for i in range(1, r):
+        for j in range(2, r):
+            if j <= i:
+                continue
+            if r % (i + j) == 0:
+                password += str(i) + str(j)
 
-for i in range(1, 51):
-    for j in range(1, 51):
-        if i != j and (i + j) % r == 0 and (i + j) != r:
-            result = i + j
-            break
-        if result:
-            break
+    return password
 
-print(f"Пара чисел, сумма которых кратна {r}: {result}")
+result = get_password(r)
+print(r)
+print('Пароль:', result)
